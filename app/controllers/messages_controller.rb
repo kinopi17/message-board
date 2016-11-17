e2a42e1ffbd0a658e5868b8597118071dfc42f75
Yasuyuki Kinoshita <kinoko0618@gmail.com>
2016-11-17 07:59:26 +0000

    delete function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+    redirect_to root_path , notice:'メッセージを編集しました'
+    else
+       render 'edit'
+    end
+  end  
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path,notice:'メッセージを削除しました'
+  end
+  
   def index
     @messages = Message.all
     @message = Message.new
   end
   
-  # 中略
+  #　中略
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -19,5 +37,9 @@ class MessagesController < ApplicationController
   
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+
+  def set_message
+  @message = Message.find(params[:id])
   end
 end
